7eb41a9cd4650dc1c7646b1dcfa735447b47c734
KotyaTheCat <kotyathecat7@gmail.com>
2020-05-27 20:03:49 +0300

    Use other script for news

---
+++ src/js/script-news.js
@@ -0,0 +1,34 @@
+const jsContainer = document.querySelector("#jsContainer");
+
+const menuBtn = document.querySelector("#menuBtn");
+const siteMenu = document.querySelector("#siteMenu");
+
+// open / close menu
+function toggleMenu() {
+  menuBtn.classList.toggle("change");
+  siteMenu.classList.toggle("shown");
+}
+  
+menuBtn.addEventListener("click", toggleMenu);
+
+// add loader to the site
+window.addEventListener(
+  'load',                                                         // when site is fully loaded
+  () => {
+    document.body.style.overflowY = "auto"
+    const preloader = document.querySelector('.preloader')
+    preloader.classList.add('preloader-finish')                 // add class to the preloader
+  }
+)
+
+// Shadow in navbar
+const navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', (e) => {
+  if(window.scrollY > window.innerHeight - 805) {
+    navbar.classList.add('navbar--shadow')
+  } else (
+    navbar.classList.remove('navbar--shadow')
+  )
+})
+
