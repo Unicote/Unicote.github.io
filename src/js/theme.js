ea04cf0006d1d27147301ba43a0351cfad376d93
KotyaTheCat <kotyathecat7@gmail.com>
2020-06-11 15:50:22 +0300

    [TheKotyaCI]: Push Built Site

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,7 +1,7 @@
 const theme=window.localStorage&&window.localStorage.getItem("theme");
-const themeToggle=document.querySelector(".theme-toggle");
+const themeToggle=document.getElementById('toggle');
 const isDark=theme==="dark";
-
+  
 var metaThemeColor=document.querySelector("meta[name=theme-color]");
 
 if(theme!==null){
@@ -9,6 +9,6 @@ document.body.classList.toggle("dark-theme",isDark);isDark?
 metaThemeColor.setAttribute("content","#252627"):metaThemeColor.setAttribute("content","#fafafa");}
 
 themeToggle.addEventListener("click",()=>{
-document.body.classList.toggle("dark-theme");window.localStorage&&window.localStorage.setItem("theme",document.body.classList.contains("dark-theme")?"dark":"light");
-document.body.classList.contains("dark-theme")?
-metaThemeColor.setAttribute("content","#252627"):metaThemeColor.setAttribute("content","#fafafa");});
+    document.body.classList.toggle("dark-theme");window.localStorage&&window.localStorage.setItem("theme",document.body.classList.contains("dark-theme")?"dark":"light");
+    document.body.classList.contains("dark-theme")?
+    metaThemeColor.setAttribute("content","#252627"):metaThemeColor.setAttribute("content","#fafafa");});
